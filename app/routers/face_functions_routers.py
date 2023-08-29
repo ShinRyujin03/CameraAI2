578510b6d4a86addb90e917eaf061c637035de67
ShinRyujin03 <duonghailong03@gmail.com>
2023-08-29 15:52:09 +0700

    function --> routers

--- app/routers/face_functions_routers.py
+++ app/routers/face_functions_routers.py
@@ -1,82 +1,101 @@
 from flask import Blueprint, jsonify, request
 from werkzeug.utils import secure_filename
-from app.sevices.face_functions import FaceLocationDetection,FaceLandmarksDetection
 from app.handle.app_error import InvalidImageError, NoImageError,DatabaseNoneError
 from database.database import Database
+import face_recognition
+import cv2
+import numpy as np
 import os
 from app.config.face_functions_config import Face_config
 # Create instances of the function classes
 
 face_router = Blueprint('face_router', __name__)
-face_detector = FaceLocationDetection()
-landmarks_detector = FaceLandmarksDetection()
+
+class FaceLocationDetection:
+    def __init__(self):
+        self.image_data = None
+
+    def facelocation(self):
+        image_np = np.frombuffer(self.image_data, np.uint8)
+        image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+        face_locations = face_recognition.face_locations(image)
+        return face_locations
+class FaceLandmarksDetection:
+    def __init__(self):
+        self.image_data = None
+
+    def facelandmarks(self):
+        image_np = np.frombuffer(self.image_data, np.uint8)
+        image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+        face_landmarks_list = face_recognition.face_landmarks(image)
+        return face_landmarks_list
+# Route for face location
+@face_router.route('/face_location', methods=['POST'])
+def get_face_location():
+        face_detector = FaceLocationDetection()
+        image_file = request.files['image']  # Access the uploaded file
+        if not image_file:
+            raise NoImageError
+
+        # Check if the uploaded file has a valid image extension
+        allowed_extensions = Face_config.path
+        filename, extension = os.path.splitext(image_file.filename)
+        if extension[1:].lower() not in allowed_extensions:
+            raise InvalidImageError
+        try:
+            # Read the image data from the file
+            image_data = image_file.read()
+            image_name = secure_filename(image_file.filename)
+            # Process the image using face_detector
+            face_detector.image_data = image_data
+            face_locations = face_detector.facelocation()
+
+            # Create a response object
+            result = {
+                'image_name': image_name,
+                'face_locations': face_locations
+            }
+            # Return the response before attempting database operations
+            db = Database()
+            db.insert_face_location(image_name, face_locations)
+            db.close_connection()
+        except:
+            raise DatabaseNoneError
+        else:
+            return jsonify(result, {"message": f"Face location metadata of {image_name} saved successfully"})
 
 
 # Route for face landmarks
 @face_router.route('/face_landmarks', methods=['POST'])
 def get_face_landmarks():
-    image_file = request.files['image']  # Access the uploaded file
-    if not image_file:
-        raise NoImageError
+        landmarks_detector = FaceLandmarksDetection()
+        image_file = request.files['image']  # Access the uploaded file
+        if not image_file:
+            raise NoImageError
 
-    # Check if the uploaded file has a valid image extension
-    allowed_extensions = Face_config.path
-    filename, extension = os.path.splitext(image_file.filename)
-    if extension[1:].lower() not in allowed_extensions:
-        raise InvalidImageError
-    try:
-        # Read the image data from the file
-        image_data = image_file.read()
-        image_name = secure_filename(image_file.filename)
-        # Process the image using landmarks_detector
-        landmarks_detector.image_data = image_data
-        landmarks = landmarks_detector.facelandmarks()
+        # Check if the uploaded file has a valid image extension
+        allowed_extensions = Face_config.path
+        filename, extension = os.path.splitext(image_file.filename)
+        if extension[1:].lower() not in allowed_extensions:
+            raise InvalidImageError
+        try:
+            # Read the image data from the file
+            image_data = image_file.read()
+            image_name = secure_filename(image_file.filename)
+            # Process the image using landmarks_detector
+            landmarks_detector.image_data = image_data
+            landmarks = landmarks_detector.facelandmarks()
 
-        # Create a response object
-        result = {
-            'image_name': image_name,
-            'landmarks': landmarks
-        }
-        db = Database()
-        db.insert_face_landmark(image_name, landmarks)
-        db.close_connection()
-    except:
-        raise DatabaseNoneError
-    else:
-        return jsonify(result, {"message": f"Face metadata of {image_name} saved successfully"})
-
-
-# Route for face location
-@face_router.route('/face_location', methods=['POST'])
-def get_face_location():
-    image_file = request.files['image']  # Access the uploaded file
-    if not image_file:
-        raise NoImageError
-
-    # Check if the uploaded file has a valid image extension
-    allowed_extensions = Face_config.path
-    filename, extension = os.path.splitext(image_file.filename)
-    if extension[1:].lower() not in allowed_extensions:
-        raise InvalidImageError
-    try:
-        # Read the image data from the file
-        image_data = image_file.read()
-        image_name = secure_filename(image_file.filename)
-        # Process the image using face_detector
-        face_detector.image_data = image_data
-        face_locations = face_detector.facelocation()
-
-        # Create a response object
-        result = {
-            'image_name': image_name,
-            'face_locations': face_locations
-        }
-        # Return the response before attempting database operations
-        db = Database()
-        db.insert_face_location(image_name, face_locations)
-        db.close_connection()
-    except:
-        raise DatabaseNoneError
-    else:
-        return jsonify(result,{"message": f"Face location metadata of {image_name} saved successfully"})
+            # Create a response object
+            result = {
+                'image_name': image_name,
+                'landmarks': landmarks
+            }
+            db = Database()
+            db.insert_face_landmark(image_name, landmarks)
+            db.close_connection()
+        except:
+            raise DatabaseNoneError
+        else:
+            return jsonify(result, {"message": f"Face metadata of {image_name} saved successfully"})
 
--- app/routers/human_detection_routers.py
+++ app/routers/human_detection_routers.py
@@ -1,17 +1,46 @@
 from flask import Blueprint, jsonify, request
 from werkzeug.utils import secure_filename
 from schema import SchemaError
-from app.sevices.human_detection import HumanDetection
+import cv2
+import numpy as np
+from ultralytics import YOLO
 from database.database import Database
 import os
 from app.handle.app_error import InvalidImageError, NoImageError,DatabaseNoneError
 from app.config.human_detection_config import Human_config
 objects_router = Blueprint('object_router', __name__)
-human_detector = HumanDetection()
+
+class HumanDetection:
+    def __init__(self):
+        self.image_data = None
+
+    def humanlocation(self):
+        # Load a pretrained YOLOv8n model
+        model = YOLO(Human_config.model_path)
+        # Convert image data to numpy array
+        image_np = np.frombuffer(self.image_data, np.uint8)
+        image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+        # Run inference on 'bus.jpg'
+        results = model(image)  # results list
+        detected_boxes = []
+        detected_weights = []
+
+        # Filter results based on the condition r.boxes.cls[i] == 0
+        for r in results:
+            cls_values = r.boxes.cls.tolist()  # Convert tensor to list
+            for i, cls in enumerate(cls_values):
+                if cls == Human_config.label_class:  # Only consider when cls is equal to 0
+                    rounded_boxes = [round(value, Human_config.round_result) for value in r.boxes.xywh[i].tolist()]
+                    detected_boxes.append(rounded_boxes)
+                    detected_weights.append(
+                        round(r.boxes.conf[i].item(), Human_config.round_result))  # Use .item() to convert scalar tensor to Python number
+
+        return detected_boxes, detected_weights
 
 
 @objects_router.route('/human_location', methods=['POST'])
 def get_human_location():
+    human_detector = HumanDetection()
     image_file = request.files['image']  # Access the uploaded file
     if not image_file:
         raise NoImageError
