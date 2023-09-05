349e758c8b40ee9185e60126bb0b036568464601
ShinRyujin03 <duonghailong03@gmail.com>
2023-09-05 11:38:07 +0700

    Revert "services added"
    This reverts commit faef3a95fc5cdd29b03e48e0f1c6717365a79fae.

--- app/services/humen_detection_services.py
+++
@@ -1,8 +0,0 @@
-from flask import request
-from app.routers.human_detection_routers import HumanDetection
-
-def request_human_location_services():
-    image_file = request.files['image']  # Access the uploaded file
-    human_location = HumanDetection()
-    human_result = human_location.get_human_location(image_file)
-    return human_result
