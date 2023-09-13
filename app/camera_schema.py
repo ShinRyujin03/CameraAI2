1140ef9a0cd34466508efa329ffd0e4f8933d5cf
ShinRyujin03 <duonghailong03@gmail.com>
2023-09-13 14:52:12 +0700

    camera_schema updated

--- app/camera_schema.py
+++ app/camera_schema.py
@@ -1,25 +1,47 @@
 from app.handle.app_error import InvalidImageError, NoImageError
 import os
 import configparser
+from pydantic import BaseModel, ValidationError
+
 # Get the current directory of your script
 script_directory = os.path.dirname(os.path.realpath("config/config.ini"))
 
 # Construct the relative path to config.ini
-config_path = os.path.join(script_directory,'config.ini')
+config_path = os.path.join(script_directory, 'config.ini')
+
 # Create a configuration object
 config = configparser.ConfigParser()
 config.read(config_path)
-pass_test = False
-image_file = None
+
+class ImageFileSchema(BaseModel):
+    datafield: str
+    datatype: str
+    mandatory: bool
+
 def schema_test(image_file):
-    if not image_file: #mandatory
+    pass_test = False
+
+    if not image_file:  # mandatory
         raise NoImageError
-    else:
-        # Check if the uploaded file has a valid image extension
-        allowed_extensions = config.get('function_config', 'path') #data field - datatype
-        filename, extension = os.path.splitext(image_file.filename)
-        if extension[1:].lower() not in allowed_extensions:
-            raise InvalidImageError
-        else:
-            pass_test = True
+
+    # Check if the uploaded file has a valid image extension
+    allowed_extensions = config.get('function_config', 'path')  # data field - datatype
+    filename, extension = os.path.splitext(image_file.filename)
+
+    if extension[1:].lower() not in allowed_extensions:
+        raise InvalidImageError
+
+    try:
+        # Validate the ImageFileSchema
+        image_data = {
+            'datafield': 'image',
+            'datatype': extension[1:].lower(),
+            'mandatory': True
+        }
+        ImageFileSchema(**image_data)
+        pass_test = True
+    except ValidationError as e:
+        # Handle validation errors here
+        pass_test = False
+
     return pass_test
