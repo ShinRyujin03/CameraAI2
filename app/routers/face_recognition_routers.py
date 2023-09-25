01a493cf7902ad129296d0ced0914116fe8d294b
ShinRyujin03 <duonghailong03@gmail.com>
2023-09-25 11:15:41 +0700

    face_recognition updated (emotions_recognition added)

--- app/routers/face_recognition_routers.py
+++ app/routers/face_recognition_routers.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from app.services.face_recognition_sevices import  FaceLandmarksDetection
+from app.services.face_recognition_sevices import  FaceLandmarksDetection,EmotionRecognition
 # Create instances of the function classes
 face_recognition_router = Blueprint('face_recognition_router', __name__)
 
@@ -10,3 +10,10 @@ def request_face_landmarks():
     face_landmarks = FaceLandmarksDetection()
     landmarks_result = face_landmarks.get_face_landmarks(image_file)
     return landmarks_result
+
+@face_recognition_router.route('/emotions_recognition', methods=['POST'])
+def request_emotions_recognition():
+    image_file = request.files['image']  # Access the uploaded file
+    face_emotions = EmotionRecognition()
+    emotions_result = face_emotions.get_emotions_recognition(image_file)
+    return emotions_result
--- app/services/face_recognition_sevices.py
+++ app/services/face_recognition_sevices.py
@@ -1,14 +1,17 @@
 import mysql
 from flask import jsonify
-from werkzeug.utils import secure_filename
-from app.schema.image_schema import *
-from database.database import Database
+import base64
 import face_recognition
 import cv2
 import numpy as np
+from deepface import DeepFace
+
+from werkzeug.utils import secure_filename
+from app.schema.image_schema import *
+from database.database import Database
 from app.handle.app_error import DatabaseNoneError, NoDetection, OutputTooLongError
-import logging
-import base64
+from app.services.face_detection_services import FaceLocationDetection
+
 
 class FaceLandmarksDetection:
     def __init__(self):
@@ -53,3 +56,66 @@ class FaceLandmarksDetection:
                     db.close_connection()
                     logging.info(result, {"message": f"Face metadata of {image_name} saved successfully"})
                     return jsonify(result, {"message": f"Face metadata of {image_name} saved successfully"})
+
+class EmotionRecognition:
+    def __init__(self):
+        self.image_data = None
+
+    def emotions_recognition(self):
+        image_np = np.frombuffer(self.image_data, np.uint8)
+        image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+
+        # Phân tích cảm xúc bằng DeepFace
+        emotions_data = DeepFace.analyze(image, actions=['emotion'], enforce_detection=False)
+
+        return emotions_data
+
+    def get_emotions_recognition(self, image_file):
+        emotions_detector = EmotionRecognition()
+        if schema_test(image_file):
+            try:
+                # Read the image data from the file
+                image_data = image_file.read()
+                base64_image = base64.b64encode(image_data)
+                base64_image_string = base64_image.decode('utf-8')
+                image_name = secure_filename(image_file.filename)
+                logging.info(f'image_name: {image_name}')
+
+                # Process the image using emotions_detector
+                emotions_detector.image_data = image_data
+                emotions_data = emotions_detector.emotions_recognition()
+
+                # Extract the emotions and probabilities for each face
+                emotions = [{'dominant_emotion': face['dominant_emotion'], 'emotion_weights': face['emotion']} for face
+                            in emotions_data]
+
+                # Get face locations using face_detector
+                face_detector = FaceLocationDetection()
+                face_detector.image_data = image_data
+                face_locations = face_detector.facelocation()
+
+                # Create a response object
+                result = {
+                    'image_name': image_name,
+                    'face_locations': face_locations,
+                    'emotions': emotions,
+                }
+                if not face_locations:  # Check if no face locations were detected
+                    logging.error(NoDetection())
+                    raise NoDetection
+                db = Database()
+            except mysql.connector.Error:
+                logging.error(DatabaseNoneError())
+                raise DatabaseNoneError
+            else:
+                if len(face_locations) > 500 or len(emotions) > 250:
+                    logging.error(OutputTooLongError())
+                    raise OutputTooLongError
+                else:
+                    # db.insert_face_landmark(image_name, emotions)
+                    # db.insert_image_file(image_name, base64_image_string)
+                    # db.close_connection()
+                    logging.info(result, {"message": f"Face emotions metadata of {image_name} saved successfully"})
+                    return jsonify(result, {"message": f"Face emotions metadata of {image_name} saved successfully"})
+
+
