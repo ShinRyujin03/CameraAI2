6c1f30248beb62b94325a9104cbfd0bb35c1ede5
ShinRyujin03 <duonghailong03@gmail.com>
2023-11-09 10:09:51 +0700

    -`increase_time` config added -Bug fixed

--- app/services/face/face_recognition_services.py
+++ app/services/face/face_recognition_services.py
@@ -93,9 +93,9 @@ class NameRecognition:
                         else:
                             low_accuracy_name.append(str(recognized_face_name))
                     if elapsed_time >= config.getint('face_function_config','recognition_elapsed_time') and increase_time_turn < config.getint('face_function_config', 'increase_time_turn'):
-                        if min_distance <= (high_accuracy_threshold + config.getfloat('face_function_config', 'delta_distance_to_high_accuracy(+)')):
-                            increase_time_turn = 1
-                            elapsed_time = elapsed_time - 10
+                        if high_accuracy_threshold < min_distance <= (high_accuracy_threshold + config.getfloat('face_function_config', 'delta_distance_to_high_accuracy(+)')):
+                            increase_time_turn += 1
+                            elapsed_time = elapsed_time - config.getint('face_function_config', 'increase_time')
                         else:
                             break
 
