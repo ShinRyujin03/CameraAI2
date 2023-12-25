20611b88657707c74b213833863149a903b8c875
Shin Ryujin <43638986+ShinRyujin03@users.noreply.github.com>
2023-12-25 10:34:11 +0700

    metadata.sql bug fixed, config training and testing set

--- database/metadata.sql
+++ database/metadata.sql
@@ -44,13 +44,14 @@ CREATE TABLE `detected_objects` (
 -- Cấu trúc bảng cho bảng `face_emotions`
 --
 
-CREATE TABLE `face_emotions` (
-  `id` int(11) NOT NULL,
-  `image_name` varchar(255) NOT NULL,
-  `face_locations` varchar(500) NOT NULL,
-  `emotions` varchar(255) NOT NULL,
-  `emotion_weights` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+-- Cấu trúc bảng cho bảng `face_facial_attribute`
+CREATE TABLE `face_facial_attribute` (
+    `id` int(11) NOT NULL,
+    `image_name` varchar(255) NOT NULL,
+    `emotions` varchar(255) NOT NULL,
+    `ages` VARCHAR(255) NOT NULL,
+    `gender` VARCHAR(255) NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
@@ -108,6 +109,8 @@ CREATE TABLE `face_metadata` (
   `face_name` varchar(255) DEFAULT NULL,
   `face_location` varchar(255) DEFAULT NULL,
   `emotions` varchar(50) DEFAULT NULL,
+  `ages` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
   `verify_status` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -165,7 +168,7 @@ ALTER TABLE `detected_objects`
 --
 -- Chỉ mục cho bảng `face_emotions`
 --
-ALTER TABLE `face_emotions`
+ALTER TABLE `face_facial_attribute`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -218,7 +221,7 @@ ALTER TABLE `detected_objects`
 --
 -- AUTO_INCREMENT cho bảng `face_emotions`
 --
-ALTER TABLE `face_emotions`
+ALTER TABLE `face_facial_attribute`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
@@ -258,72 +261,93 @@ ALTER TABLE `image`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 COMMIT;
 
-CREATE TRIGGER `after_insert_face_emotions` AFTER INSERT ON `face_facial_attribute`
- FOR EACH ROW BEGIN
+-- Trigger for face_facial_attribute
+DELIMITER //
+
+CREATE TRIGGER after_insert_face_emotions
+AFTER INSERT ON face_facial_attribute
+FOR EACH ROW
+BEGIN
     INSERT INTO face_metadata (image_name, emotions)
     VALUES (NEW.image_name, NEW.emotions)
     ON DUPLICATE KEY UPDATE emotions = NEW.emotions;
-END
+END //
 
-CREATE TRIGGER `after_insert_face_location` AFTER INSERT ON `face_location`
- FOR EACH ROW BEGIN
+CREATE TRIGGER after_insert_face_location
+AFTER INSERT ON face_location
+FOR EACH ROW
+BEGIN
     INSERT INTO face_metadata (image_name, face_location)
     VALUES (NEW.image_name, NEW.face_location)
     ON DUPLICATE KEY UPDATE face_location = NEW.face_location;
-END
+END //
 
-CREATE TRIGGER `after_insert_face_verified` AFTER INSERT ON `face_verified`
- FOR EACH ROW BEGIN
+CREATE TRIGGER after_insert_face_verified
+AFTER INSERT ON face_verified
+FOR EACH ROW
+BEGIN
     INSERT INTO face_metadata (image_name, verify_status)
     VALUES (NEW.image_name, NEW.verify_status)
     ON DUPLICATE KEY UPDATE verify_status = NEW.verify_status;
-END
+END //
 
-CREATE TRIGGER `after_insert_image` AFTER INSERT ON `image`
- FOR EACH ROW BEGIN
+CREATE TRIGGER after_insert_image
+AFTER INSERT ON image
+FOR EACH ROW
+BEGIN
     INSERT INTO face_metadata (image_name, image_file)
     VALUES (NEW.image_name, NEW.image_file)
     ON DUPLICATE KEY UPDATE image_file = NEW.image_file;
-END
+END //
 
-CREATE TRIGGER `insert_gender_age` AFTER INSERT ON `face_facial_attribute`
- FOR EACH ROW BEGIN
+-- Trigger for face_facial_attribute
+CREATE TRIGGER insert_gender_age
+AFTER INSERT ON face_facial_attribute
+FOR EACH ROW
+BEGIN
     INSERT INTO face_metadata (image_name, gender, ages)
     VALUES (NEW.image_name, NEW.gender, NEW.ages)
-    ON DUPLICATE KEY UPDATE
-        gender = NEW.gender,
-        ages = NEW.ages;
-END
+    ON DUPLICATE KEY UPDATE gender = NEW.gender, ages = NEW.ages;
+END //
 
-CREATE TRIGGER `update_face_emotions` AFTER INSERT ON `face_facial_attribute`
- FOR EACH ROW BEGIN
+CREATE TRIGGER update_face_emotions
+AFTER INSERT ON face_facial_attribute
+FOR EACH ROW
+BEGIN
     INSERT INTO face_metadata (image_name, emotions)
     VALUES (NEW.image_name, NEW.emotions)
     ON DUPLICATE KEY UPDATE emotions = NEW.emotions;
-END
+END //
 
-CREATE TRIGGER `update_face_location` AFTER INSERT ON `face_location`
- FOR EACH ROW BEGIN
+CREATE TRIGGER update_face_location
+AFTER INSERT ON face_location
+FOR EACH ROW
+BEGIN
     INSERT INTO face_metadata (image_name, face_location)
     VALUES (NEW.image_name, NEW.face_location)
     ON DUPLICATE KEY UPDATE face_location = NEW.face_location;
-END
+END //
 
-CREATE TRIGGER `update_face_verified` AFTER INSERT ON `face_verified`
- FOR EACH ROW BEGIN
+CREATE TRIGGER update_face_verified
+AFTER INSERT ON face_verified
+FOR EACH ROW
+BEGIN
     INSERT INTO face_metadata (image_name, face_name, verify_status)
     VALUES (NEW.image_name, NEW.face_name, NEW.verify_status)
     ON DUPLICATE KEY UPDATE face_name = NEW.face_name, verify_status = NEW.verify_status;
-END
+END //
 
-CREATE TRIGGER `update_gender_age` AFTER UPDATE ON `face_facial_attribute`
- FOR EACH ROW BEGIN
+CREATE TRIGGER update_gender_age
+AFTER UPDATE ON face_facial_attribute
+FOR EACH ROW
+BEGIN
     UPDATE face_metadata
-    SET
-        gender = NEW.gender,
-        ages = NEW.ages
+    SET gender = NEW.gender, ages = NEW.ages
     WHERE image_name = NEW.image_name;
-END
+END //
+
+DELIMITER ;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
